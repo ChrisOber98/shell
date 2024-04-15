e16eea279162a2248bf4b36854378ef1b6702cfc
chrisober98@gmail.com <chrisober@Chriss-MacBook-Pro.local>
2024-04-14 22:51:28 -0400

    Added Basic Function to repeat prompt and exit when exit command is entered
    Signed-off-by: chrisober98@gmail.com <chrisober@Chriss-MacBook-Pro.local>

--- shell.c
+++ shell.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define INPUT_SIZE 2048
 
 int main(int argc, char* argv[])
 {
-	printf("Hello Word!\n");
+	//Variable to hold user input.
+	char input[INPUT_SIZE];
+
+	do
+	{
+		printf("..< ");
+		fgets(input, INPUT_SIZE, stdin);
+		printf("You entered: %s\n", input);
+
+	}while(strcmp("exit", input) != 0);
 
 	return 0;
 }
