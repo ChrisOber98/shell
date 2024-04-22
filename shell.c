65f7373c6e354824498c8d56df5448ae07310351
chrisober98@gmail.com <chrisober@Chriss-MBP.lan>
2024-04-22 14:44:12 -0400

    Added both parsing to the input reading to store as an argument vector as well as function handling for both builtin commands as well as standard commands
    Signed-off-by: chrisober98@gmail.com <chrisober@Chriss-MBP.lan>

--- shell.c
+++ shell.c
@@ -2,8 +2,13 @@
 
 int main(int argc, char ** argv)
 {
+	// Load config files, if any.
 
+
+	// Run command loop.
 	shell_loop();
+
+	//  Perform any shutdown or cleanup.
 
 	return EXIT_SUCCESS;
 }
--- shell.h
+++ shell.h
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 #define SHELL_RL_BUFSIZE 1024
 
@@ -45,21 +47,171 @@ char * shell_read_line(void)
 	}
 }
 
+#define SHELL_TOK_BUFSIZE 64
+#define SHELL_TOK_DELIM " \t\r\n\a"
+
+char ** shell_split_line(char * line)
+{
+	int bufsize = SHELL_TOK_BUFSIZE;
+	int position = 0;
+
+	char **tokens = malloc(bufsize * sizeof(char*));
+	char *token;
+
+	if (!tokens)
+	{
+		fprintf(stderr, "shell: allocation error\n");
+		exit(EXIT_FAILURE);
+	}
+
+	token = strtok(line, SHELL_TOK_DELIM);
+	while (token != NULL)
+	{
+		tokens[position] = token;
+		position++;
+
+		if(position >= bufsize)
+		{
+			bufsize += SHELL_TOK_BUFSIZE;
+			tokens = realloc(tokens, bufsize * sizeof(char*));
+			if (!tokens)
+			{
+				fprintf(stderr, "shell: allocation error\n");
+				exit(EXIT_FAILURE);
+			}
+		}
+		token = strtok(NULL, SHELL_TOK_DELIM);
+	}
+	tokens[position] = NULL;
+	return tokens;
+}
+
+int shell_launch(char ** args)
+{
+	pid_t pid;
+	pid_t wpid;
+
+	int status;
+
+	pid = fork();
+	if (pid == 0)
+	{
+		if (execvp(args[0], args) == -1)
+		{
+			perror("shell");
+		}
+		exit(EXIT_FAILURE);
+	}
+	else if (pid < 0)
+	{
+		perror("shell");
+	}
+	else
+	{
+		do
+		{
+			wpid = waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+
+	return 1;
+}
+
+/*
+  Function Declarations for builtin shell commands:
+ */
+int shell_cd(char **args);
+int shell_help(char **args);
+int shell_exit(char **args);
+
+/*
+  List of builtin commands, followed by their corresponding functions.
+ */
+char *builtin_str[] = {
+  "cd",
+  "help",
+  "exit"
+};
+
+int (*builtin_func[]) (char **) = {
+  &shell_cd,
+  &shell_help,
+  &shell_exit
+};
+
+int shell_num_builtins() {
+  return sizeof(builtin_str) / sizeof(char *);
+}
+
+/*
+  Builtin function implementations.
+*/
+int shell_cd(char **args)
+{
+  if (args[1] == NULL) {
+    fprintf(stderr, "shell: expected argument to \"cd\"\n");
+  } else {
+    if (chdir(args[1]) != 0) {
+      perror("shell");
+    }
+  }
+  return 1;
+}
+
+int shell_help(char **args)
+{
+  int i;
+  printf("Chris Obers' Shell\n");
+  printf("Type program names and arguments, and hit enter.\n");
+  printf("The following are built in:\n");
+
+  for (i = 0; i < shell_num_builtins(); i++) {
+    printf("  %s\n", builtin_str[i]);
+  }
+
+  printf("Use the man command for information on other programs.\n");
+  return 1;
+}
+
+int shell_exit(char **args)
+{
+  return 0;
+}
+
+int shell_execute(char **args)
+{
+  int i;
+
+  if (args[0] == NULL) {
+    // An empty command was entered.
+    return 1;
+  }
+
+  for (i = 0; i < shell_num_builtins(); i++) {
+    if (strcmp(args[0], builtin_str[i]) == 0) {
+      return (*builtin_func[i])(args);
+    }
+  }
+
+  return shell_launch(args);
+}
+
+
+
 void shell_loop()
 {
 	char * line;
-	// char ** args;
+	char ** args;
 	int status;
 
 	do 
 	{
 		printf("> ");
 		line = shell_read_line();
-		printf("%s\n", line);
-		// args = shell_split_line(line);
-		// status = shell_execute(args);
+		args = shell_split_line(line);
+		status = shell_execute(args);
 
 		free(line);
-		// free(args);
+		free(args);
 	} while (status);
 }
